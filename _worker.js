815973fb4dfb42fa7dc2818e213fca8b6f9e6dce
Davood Qorbani <DavoodQorbani@users.noreply.github.com>
2024-05-12 20:54:24 +0200

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -40,7 +40,7 @@ export default {
             
             if (!upgradeHeader || upgradeHeader !== 'websocket') {
                 
-                const url = new URL(request.url);
+                //const url = new URL(request.url);
                 const searchParams = new URLSearchParams(url.search);
                 const host = request.headers.get('Host');
                 const client = searchParams.get('app');
@@ -342,7 +342,7 @@ async function handleTCPOutBound(request, remoteSocket, addressRemote, portRemot
 	 * @returns {Promise<void>} A Promise that resolves when the retry is complete.
 	 */
 	async function retry() {
-        //const { pathname } = new URL(request.url);
+        const { pathname } = new URL(request.url);
         let panelProxyIP = pathname.split('/')[2];
         panelProxyIP = panelProxyIP ? atob(panelProxyIP) : undefined;
 		const tcpSocket = await connectAndWrite(panelProxyIP || proxyIP || addressRemote, portRemote);
