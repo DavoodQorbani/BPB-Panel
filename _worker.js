e7a02dd1ec581e2968770a3d70cf8b6fd76053a4
Davood Qorbani <DavoodQorbani@users.noreply.github.com>
2024-07-20 21:31:46 +0200

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -827,12 +827,12 @@ const generateRemark = (index, port) => {
         //case 1:
             //remark = `💦 BPB - Domain_${index + 1} : ${port}`;
             //break;
-        //case 2:
+        case 2:
         case 3:
             remark = `🧩 خودمون🇺🇸آمریکا - IPv4_${index + 1}`;
             break;
         case 4:
-        case 5:
+        //case 5:
             //remark = `💦 BPB - IPv6_${index - 3} : ${port}`;
             //break;
         default:
